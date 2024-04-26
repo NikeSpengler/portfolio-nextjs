b2a82e9ba10c148eced6463d9a342a599a00c200
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2024-04-26 16:17:10 +0200

    added style for sidebar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React, {useState, useEffect} from 'react';
-// component
+// components
 import ThemeToggler from "./ThemeToggler"
 import Sidebar from './Sidebar';
 
@@ -8,11 +8,10 @@ const Header = () => {
   return (
     <header>
         <div className='container mx-auto'>
-            <div className='flex justify-between items-center pt-2'>
+            <div className='flex justify-between items-center pt-4 pl-4 pr-4'>
                 <ThemeToggler />
                 <div className='flex items-center'>
-                    
-                    <Sidebar />
+                    <Sidebar containerStyles="gap-x-8 items-center"/>
                 </div>
             </div>
         </div>
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,15 +1,46 @@
 import {Sheet, SheetContent, SheetTrigger} from '@/components/ui/sheet'
-import {AlignJustify} from 'lucide-react'
+import {AlignJustify} from 'lucide-react';
+import {motion} from 'framer-motion';
 
-const Sidebar = () => {
+// Links (next.js)
+import Link from "next/link"
+
+// Next hooks
+import { usePathname } from 'next/navigation';
+
+const links = [
+    {path: '/', name: 'about'},
+    {path: '/work', name: 'work'},
+    {path: '/contact', name: 'contact'},
+];
+
+const Sidebar = ({containerStyles, linkStyles, underlineStyles }) => {
+    const path = usePathname()
   return (
-    <Sheet>
+    <Sheet className={`${containerStyles}`}>
         <SheetTrigger>
             <AlignJustify className=''/>
         </SheetTrigger>
-        <SheetContent>
-            Sidebar
-        </SheetContent>
+        <div className='flex flex-col items-center gap-y-6'>
+            <SheetContent className='flex flex-col items-center gap-y-6 pt-[200px]'>
+            
+                    {links.map((link, index) => {
+                        return (
+                        <Link 
+                            href={link.path} 
+                            key={index}
+                            className={`capitalize ${linkStyles}`}
+                            >
+                            {link.path === path && (
+                                <motion.span initial={{y: '-100'}}/>
+                            )}
+                            {link.name}
+                        </Link>
+                        );
+                    })}
+            
+            </SheetContent>
+        </div>
     </Sheet>
   )
 }
