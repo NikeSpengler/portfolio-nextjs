7b224c3a5778e54a3b0f2aa943a362ac6b480c77
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2024-04-30 14:05:38 +0200

    Added som text and style to the Hero page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,35 @@
 import Link from 'next/link';
+import  {Button} from './ui/button';
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 ng-hero bg-no-repeat 
+    bg-bottom bg-cover dark:bg-none'>
+        <div className='container mx-auto'>
+            <div className='flex justify-between gap-x-8'>
+                <div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0
+                text-center m:text-left xl:text-left'>
+                    <div className='text-sm mb-4 tracking-[1px]'>FULLSTACK DEVELOPER</div>
+                    <h1 className='h1'>Nike Spengler</h1>
+                    <p className='subtitle max-w-[490px] pt-4 mx-auto xl:mx-0'>
+                        I am a fullstack developer with a keen intrest in and focus on Frontend Development. 
+                    </p>
+                </div>
+               
+                {/* Image */}
+                <div className='hidden xl:flex relative'>
+                    Img
+                </div>
+            </div>
+             {/* Button */}
+             <div>
+                    <Link href={'/contact'}>
+                        <button>Read more</button>
+                    </Link>
+                </div>
+        </div>
+    </section>
+    
   )
 }
 
